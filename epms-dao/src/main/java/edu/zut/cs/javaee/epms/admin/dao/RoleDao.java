02529aea8cb437aa70c156b00a1cd770f0fc4f4d
Fox1411 <1096083178@qq.com>
2018-05-17 20:58:07 +0800

    new one

---
+++ epms-dao/src/main/java/edu/zut/cs/javaee/epms/admin/dao/RoleDao.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.javaee.dream.admin.dao;
+
+import edu.zut.cs.javaee.dream.admin.domain.Role;
+import edu.zut.cs.javaee.dream.base.dao.GenericDao;
+
+public interface RoleDao extends GenericDao<Role, Long> {
+
+}
---
+++ epms-dao/src/main/java/edu/zut/cs/javaee/epms/admin/dao/UserDao.java
@@ -0,0 +1,7 @@
+package edu.zut.cs.javaee.dream.admin.dao;
+
+import edu.zut.cs.javaee.dream.admin.domain.User;
+import edu.zut.cs.javaee.dream.base.dao.GenericDao;
+
+public interface UserDao extends GenericDao<User, Long> {
+}
