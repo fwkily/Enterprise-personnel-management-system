568dc96cee6f5d8754125fff2a2178351c9d34a2
Fox1411 <1096083178@qq.com>
2018-06-09 23:35:56 +0800

    new

---
+++ epms-dao/src/main/java/edu/zut/cs/javaee/epms/admin/dao/GroupDao.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.javaee.epms.admin.dao;
+
+import edu.zut.cs.javaee.epms.admin.domain.Group;
+import edu.zut.cs.javaee.epms.base.dao.GenericTreeDao;
+
+public interface GroupDao extends GenericTreeDao<Group, Long> {
+	
+}
