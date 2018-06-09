080c54b740bacf18788148147627626d671c0291
llf113355 <443850494@qq.com>
2018-06-09 19:46:29 +0800

    aaa

---
+++ epms-dao/src/main/java/edu/zut/cs/javaee/epms/ensure/dao/EnsureDao.java
@@ -0,0 +1,9 @@
+package edu.zut.cs.javaee.epms.ensure.dao;
+
+import edu.zut.cs.javaee.epms.base.dao.GenericDao;
+import edu.zut.cs.javaee.epms.ensure.domain.Ensure;
+import edu.zut.cs.javaee.epms.module.domain.Module;
+
+public interface EnsureDao extends GenericDao<Ensure, Long> {
+
+}
