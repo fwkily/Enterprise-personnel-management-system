564e38a23e97fa50b20e6a01299a01a2d891338b
zhougenhui <852602555@qq.com>
2018-06-09 18:31:06 +0800

    new pay

---
+++ epms-dao/src/main/java/edu/zut/cs/javaee/epms/pay/dao/PayDao.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.javaee.epms.pay.dao;
+
+import edu.zut.cs.javaee.epms.base.dao.GenericDao;
+import edu.zut.cs.javaee.epms.pay.domain.Pay;
+
+public interface PayDao extends GenericDao<Pay, Long> {
+
+}
