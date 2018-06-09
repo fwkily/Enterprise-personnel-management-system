76842f1db89717833776a195e01fa2b40023daaf
zhougenhui <852602555@qq.com>
2018-06-09 18:47:47 +0800

    new pay.domain

---
+++ epms-dao/src/main/java/edu/zut/cs/javaee/epms/pay/domain/Pay_.java
@@ -0,0 +1,14 @@
+package edu.zut.cs.javaee.epms.pay.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2018-05-24T09:01:25.954+0800")
+@StaticMetamodel(Pay.class)
+public class Pay_ {
+	public static volatile SingularAttribute<Pay, String> name;
+	public static volatile SingularAttribute<Pay, String> work;
+	public static volatile SingularAttribute<Pay, String> time;
+	public static volatile SingularAttribute<Pay, String> money;
+}
