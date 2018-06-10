ef40870b48fc1a7c6a8557188c2af8b0bfd69ff5
Fox1411 <1096083178@qq.com>
2018-06-10 23:08:42 +0800

    new dao

---
+++ epms-dao/src/main/java/edu/zut/cs/javaee/epms/admin/dao/UserDao.java
@@ -0,0 +1,7 @@
+package edu.zut.cs.javaee.epms.admin.dao;
+
+import edu.zut.cs.javaee.epms.admin.domain.User;
+import edu.zut.cs.javaee.epms.base.dao.GenericDao;
+
+public interface UserDao extends GenericDao<User, Long> {
+}
