0a4cada91a4ab655df3ee72b0cfa2ddd748974bf
Fox1411 <1096083178@qq.com>
2018-06-10 11:09:20 +0800

    roledao

---
+++ epms-dao/src/main/java/edu/zut/cs/javaee/epms/admin/dao/RoleDao.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.javaee.epms.admin.dao;
+
+import edu.zut.cs.javaee.epms.admin.domain.Role;
+import edu.zut.cs.javaee.epms.base.dao.GenericDao;
+
+public interface RoleDao extends GenericDao<Role, Long> {
+
+}
