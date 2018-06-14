2ab3ee6c8f39b804e0c094bf94f55c243e13dc7d
Charles <Charles@DESKTOP-R7HKSOD>
2018-06-14 08:46:33 +0800

    更新module

---
+++ epms-dao/src/main/java/edu/zut/cs/javaee/epms/module/dao/ModuleDao.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.javaee.epms.module.dao;
+
+import edu.zut.cs.javaee.epms.base.dao.GenericDao;
+import edu.zut.cs.javaee.epms.module.domain.Module;
+
+public interface ModuleDao extends GenericDao<Module, Long> {
+
+}
