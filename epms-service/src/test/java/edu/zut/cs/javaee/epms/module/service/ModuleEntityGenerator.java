e047ea2d6f58a8d0b2584f9c2657b11ff89e91aa
Charles <Charles@DESKTOP-R7HKSOD>
2018-06-15 14:55:25 +0800

    update_module

---
+++ epms-service/src/test/java/edu/zut/cs/javaee/epms/module/service/ModuleEntityGenerator.java
@@ -0,0 +1,28 @@
+package edu.zut.cs.javaee.epms.module.service;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import  edu.zut.cs.javaee.epms.module.domain.Module;
+import edu.zut.cs.javaee.epms.base.service.GenericGenerator;
+
+public class ModuleEntityGenerator extends GenericGenerator {
+
+	@Autowired
+	ModuleManager moduleManager;
+
+	@Test
+	public void gen_module() {
+		for (int i = 0; i < 10; i++) {
+			Module g = new Module();
+			g.setName("Module_" + i);
+			this.moduleManager.save(g);
+			for (int j = 0; j < 10; j++) {
+				Module module = new Module();
+				module.setName("module_" + i + "_" + j);
+				module.setParent(g);
+				g = this.moduleManager.save(module);
+			}
+		}
+	}
+
+}
