93f0d9a7e10bd146eaff658dc1ed584f9c0a1858
Fox1411 <1096083178@qq.com>
2018-07-03 17:27:02 +0800

    new change

--- epms-web/src/main/java/edu/zut/cs/javaee/epms/admin/web/spring/controller/UserController.java
+++ epms-web/src/main/java/edu/zut/cs/javaee/epms/admin/web/spring/controller/UserController.java
@@ -3,20 +3,21 @@ package edu.zut.cs.javaee.epms.admin.web.spring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.zut.cs.javaee.epms.admin.domain.User;
 import edu.zut.cs.javaee.epms.admin.service.UserManager;
 import edu.zut.cs.javaee.epms.base.web.spring.controller.GenericController;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/admin")
 public class UserController extends GenericController<User, Long, UserManager> {
 
     UserManager userManager;
 
-    @RequestMapping("/index")
+    @RequestMapping(method = RequestMethod.GET, value = "/index.html")
     public String index() {
-        return "user/index";
+        return "/admin/index_user";
     }
 
     @Autowired
